6d7ec9470579841d7c1394cf742ed53a2e1ce5b1
rmaderom <rodrigomadero@me.com>
2019-04-01 18:52:51 -0400

    Update volcano.ino
    started adding stuff for fan motor

--- volcano/volcano.ino
+++ volcano/volcano.ino
@@ -1,29 +1,39 @@
 #include <Servo.h>
 
-Servo servoBoi;  // name our servo
+Servo wandDipper;  // name our servo
+Servo fanBlower; //name of motor that spins fan
 
-int pos = 0;  // initialize variable for servo's position
-int servoPin = 9; // servo's wire on 'duino
+int wandDipperPos = 0;  // initialize variable for wandDipper's position
+int fanBlowerPos = 0; // initialize variable for fanBlower's position
+int wandDipperPin = 9; // wandDipper's wire on 'duino
+int fanBlowerPin = 10; // fanBlower's wire on 'duino
 int delayTimeInc = 30; //delay time for each move of one degree in ms
 int delayTimeEnd = 1000; // delay time once servo reaches 180 degrees to allow time for bubbles to be blown
 
+
 void setup() {
   Serial.begin(9600);
-  servoBoi.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  wandDipper.attach(wandDipperPin);  // attaches
+  fanBlower.attach(fanBlowerPin);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // servo rotates from 0 to 180 degrees in increments of one degree
-    servoBoi.write(pos);              // servo will move to position specified by variable pos
-    delay(delayTimeInc);                       // waits 15ms for the servo to reach the position
-    Serial.println(pos);
+  // wandDipper stuff
+  for (wandDipperPos = 0; wandDipperPos <= 180; wandDipperPos += 1) { // servo rotates from 0 to 180 degrees in increments of one degree
+    wandDipper.write(wandDipperPos);              // servo will move to wandDipperPosition specified by variable wandDipperPos
+    delay(delayTimeInc);                       // waits for the servo to reach the wandDipperPosition
+    Serial.println(wandDipperPos);
   }
   
-  delay(delayTimeEnd); //wait for bubbles ot be blown by fan
+  delay(delayTimeEnd); //wait for bubbles to be blown by fan
   
-  for (pos = 180; pos >= 0; pos -= 1) { // servo rotates from 180 to 0 degrees in increments of one degree
-    servoBoi.write(pos);              // servo will move to position specified by variable pos
-    delay(delayTimeInc);                       // waits 15ms for the servo to reach the position
-    Serial.println(pos);
+  for (wandDipperPos = 180; wandDipperPos >= 0; wandDipperPos -= 1) { // servo rotates from 180 to 0 degrees in increments of one degree
+    wandDipper.write(wandDipperPos);              // servo will move to wandDipperPosition specified by variable wandDipperPos
+    delay(delayTimeInc);                       // waits for the servo to reach the wandDipperPosition
+    Serial.println(wandDipperPos);
   }
+
+  //fanBlower stuff
+  fanBlower.write(180);
+    
 }
